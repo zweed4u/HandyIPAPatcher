0b615ec84cdef107ba5fed540edd373cadc6b30d
karek314 <karek314@protonmail.ch>
2018-04-16 23:54:55 +0200

    Fix "AppName" and more
    Fixed "app name" logical issue
    Fixed issue when auto grab filed due ipa name with whitespaces.

--- extract_thin.sh
+++ extract_thin.sh
@@ -1,23 +1,30 @@
 #!/bin/bash
+clear; printf '\033[3J'
 BASE_IPA=app.ipa
+FRAMEWORKS_EXTRACT=false
+if [ -d "screenshots" ]; then
+	rm -R screenshots
+fi
 echo -e "\033[1mExtract Thin Started\033[0m"
-if [ $# -eq 0 ]
-  then
-    echo -e "\033[4mSpecify binary/app name, case sensitivite.\033[0m"
-    echo ""
+if [ "$1" = "help" ]
+	then
     echo -e "\033[2mBasic Usage\033[0m"
-    echo -e "\033[1mbash extract_thin.sh AppName\033[0m"
+    echo -e "\033[1mbash extract_thin.sh\033[0m"
     echo ""
     echo -e "\033[2mExtract with frameworks\033[0m"
-    echo -e "\033[1mbash extract_thin.sh AppName frameworks\033[0m"
+    echo -e "\033[1mbash extract_thin.sh frameworks\033[0m"
     echo ""
     exit
+elif [ "$1" = "frameworks" ]
+	then
+	FRAMEWORKS_EXTRACT=true
 fi
 if [ ! -f $BASE_IPA ]
 then
   echo "There is no app.ipa in this directory, trying to grab automatically"
-  IPA="$(ls *.ipa | head -1)"
-  cp $IPA $BASE_IPA
+  IPA="$(find . -not -path '*/\.*' -not -path './orginal_app.ipa' -type f -name '*.ipa*' | awk -F / '{print $2}')"
+  echo "Found: $IPA"
+  cp "$IPA" "$BASE_IPA"
 fi
 if [ -d "tmp" ]
   then
@@ -27,32 +34,50 @@ echo "Creating temporary work dir"
 mkdir tmp
 echo "Unzipping ipa file"
 unzip -o app.ipa -d tmp
-if [ "$2" = "frameworks" ]
-  then
-  echo "Extracting framework binaries"
-  if [ -d "tmp/Payload/$1.app/Frameworks" ]
-    then
-    if [ ! -d "tmp_frameworks" ]
-      then
-        mkdir tmp_frameworks
-    fi
-    cd tmp/Payload/$1.app/Frameworks
-    for filename in *; do
-      binary=`echo "$filename" | cut -d'.' -f1`
-      echo $binary
-      cp $filename/$binary ../../../../tmp_frameworks/$binary
-      echo "Extracting thin from fat"
-      lipo -thin armv7 ../../../../tmp_frameworks/$binary -o "../../../../tmp_frameworks/$binary"_armv7
-      lipo -thin arm64 ../../../../tmp_frameworks/$binary -o "../../../../tmp_frameworks/$binary"_arm64
-    done
-    cd ..;cd ..;cd ..;cd ..
-  else
-    echo "No frameworks detected"
-  fi
+BINARY_NAME=$(ls tmp/Payload/ | tail -1)
+if [[ $BINARY_NAME != *".app"* ]]
+  	then
+  	echo "Can't get .app name - shouldn't happen!"
+  	echo "File name: $BINARY_NAME"
 fi
-lipo -thin armv7 tmp/Payload/$1.app/$1 -o $1_armv7
-lipo -thin arm64 tmp/Payload/$1.app/$1 -o $1_arm64
+BINARY_NAME=$(echo $BINARY_NAME | awk -F . '{print $1}')
+echo "Binary name: $BINARY_NAME"
+if [ "$FRAMEWORKS_EXTRACT" = true ]
+	then
+  	echo "Extracting framework binaries"
+  	if [ -d "tmp/Payload/$BINARY_NAME.app/Frameworks" ]
+    	then
+    	cd tmp/Payload/$BINARY_NAME.app/Frameworks
+       	FRAMEWORK_COUNT=$(ls | wc -l | xargs)
+    	echo "Frameworks count: $FRAMEWORK_COUNT"
+    	if [[ "$FRAMEWORK_COUNT" != "0" ]]; then
+    		cd ..;cd ..;cd ..;cd ..
+    		if [ ! -d "tmp_frameworks" ]
+      			then
+       			mkdir tmp_frameworks
+    		fi
+    		cd tmp/Payload/$BINARY_NAME.app/Frameworks
+    		for filename in *; do
+      			binary=`echo "$filename" | cut -d'.' -f1`
+      			echo $binary
+      			cp $filename/$binary ../../../../tmp_frameworks/$binary
+      			echo "Extracting thin from fat"
+      			lipo -thin armv7 ../../../../tmp_frameworks/$binary -o "../../../../tmp_frameworks/$binary"_armv7
+      			lipo -thin arm64 ../../../../tmp_frameworks/$binary -o "../../../../tmp_frameworks/$binary"_arm64
+    		done
+    	else
+    		echo "No frameworks detected"
+    	fi
+    	cd ..;cd ..;cd ..;cd ..
+  	else
+    	echo "No frameworks detected"
+  	fi
+fi
+lipo -thin armv7 tmp/Payload/$BINARY_NAME.app/$BINARY_NAME -o "$BINARY_NAME"_armv7
+lipo -thin arm64 tmp/Payload/$BINARY_NAME.app/$BINARY_NAME -o "$BINARY_NAME"_arm64
 echo "Extracted thin from fat"
 echo "Cleaning workspace..."
 rm -R tmp
 echo "Done. Now you can work on thin binaries"
+echo -e "\033[2mList of files\033[0m"
+ls
